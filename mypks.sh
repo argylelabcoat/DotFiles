4e5c971f456ee8fb500cc9be470d840d23738d6c
Matthew Hughes <mhughes@rtautomation.com>
2020-07-07 10:37:11 -0500

    Updating base script TODO: still need to update my .config / etc

--- mypks.sh
+++ mypks.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install zsh dkms vim-gtk rxvt-unicode-256color openjdk-7-jdk openjdk-7-jre gcc clang geany \
-python-pip scons ruby subversion subversion-tools mercurial git curl exuberant-ctags 
+sudo apt-get install zsh fish rxvt-unicode-256color gcc clang neovim \
+python3-pip scons cmake subversion subversion-tools fossil mercurial git exuberant-ctags \
+rsync rclone rclone-gui curl wget \
+picom numlockx flatpak i3 i3bar i3lock yabar rofi alacritty \
+zathura xfe ranger udiskie \
+mpd mpc cantada pms pamix pactl \
+
+
+pip3 install conan meson tmuxp httpie git-archive-all
 
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh 
-chsh -s `which zsh`
+chsh -s `which fish`
 cp .vimrc ~/.vimrc
 cp -r .vim ~/.vim
 cp -r .fonts ~/.fonts
