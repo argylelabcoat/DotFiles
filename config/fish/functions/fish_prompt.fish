0883f22929d020e37f90940ced85c5c50f3a69b5
Matthew Hughes <mhughes@rtautomation.com>
2020-08-14 15:55:56 -0500

    Adding tmux conf override putting newline in fish prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt
-  eval powerline-go -error $status -shell bare -modules venv,ssh,cwd,perms,git,hg,fossil,time,root
+  eval powerline-go -newline -error $status -shell bare -modules venv,ssh,cwd,perms,git,hg,fossil,time,root
 end
