2d26df307985521208e7d69e9e1cc8d6b434baa3
Matthew Hughes <mhughes@rtautomation.com>
2020-08-26 16:46:03 -0500

    Shell Setup

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,3 +1,4 @@
 function fish_prompt
-  eval powerline-go -newline -error $status -shell bare -modules venv,ssh,cwd,perms,git,hg,fossil,time,root
+  set duration (math -s6 "$CMD_DURATION / 1000")
+  eval powerline-go -condensed -error $status -duration $duration -shell bare -newline -modules cwd,docker,dotenv,venv,plenv,fossil,git,hg,svn,jobs,node,vgo,duration
 end
