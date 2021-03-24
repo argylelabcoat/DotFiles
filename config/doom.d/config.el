6d45ed8bbe8860f85d127ee1072cf93be5c08474
Matthew Hughes <mhughes@rtautomation.com>
2021-03-24 17:19:39 -0500

    Today's updates

--- config/doom.d/config.el
+++ config/doom.d/config.el
@@ -25,8 +25,10 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
-(setq doom-font "Hack NF")
+(setq doom-theme 'doom-opera-light)
+(setq doom-font (font-spec :family "ProFontIIx Nerd Font Mono" :size 12)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 12))
+
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
--- config/doom.d/packages.el
+++ config/doom.d/packages.el
@@ -11,6 +11,7 @@
 
 (package! adoc-mode)
 (add-to-list 'auto-mode-alist (cons "\\.adoc\\'" 'adoc-mode))
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
